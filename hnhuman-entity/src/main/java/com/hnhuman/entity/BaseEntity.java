04dd7487f073dec1301a9b2d49f51d9dfa2d7650
15618910786 <candy.tam@aliyun.com>
2015-08-13 18:09:11 +0800

    初始化项目.

--- hnhuman-entity/src/main/java/com/hnhuman/entity/BaseEntity.java
+++ hnhuman-entity/src/main/java/com/hnhuman/entity/BaseEntity.java
@@ -2,4 +2,7 @@ package com.hnhuman.entity;
 
 public class BaseEntity {
 
+	/** "创建日期"属性名称 */
+	public static final String CREATE_DATE_PROPERTY_NAME = "createDate";
+	
 }
---
+++ hnhuman-entity/src/main/java/com/hnhuman/entity/OrderEntity.java
@@ -0,0 +1,8 @@
+package com.hnhuman.entity;
+
+public class OrderEntity extends BaseEntity {
+
+	/** "排序"属性名称 */
+	public static final String ORDER_PROPERTY_NAME = "order";
+	
+}
