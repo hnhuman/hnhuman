1542316a8128204089426974a1dc57dfdbfd2785
15618910786 <candy.tam@aliyun.com>
2015-09-16 18:20:47 +0800

    初始化项目.

--- hnhuman-entity/src/main/java/com/hnhuman/entity/Human.java
+++ hnhuman-entity/src/main/java/com/hnhuman/entity/Human.java
@@ -1,6 +1,8 @@
 package com.hnhuman.entity;
 
 
+import java.util.Date;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -10,21 +12,39 @@ import javax.persistence.Id;
 @Entity(name="hnhuman")
 public class Human {
 	
-	/** ID */
-	private Long id;
-	/**
-	 * 获取ID
-	 * 
-	 * @return ID
-	 */
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	
+	@Column
+	private String name;
+	
+	@Column
+	private String mobile;
+	
+	@Column
+	private String email;
+	
+	@Column
+	private String avatar;
+
+	@Column
+	private Gender gender;
+	
+	@Column
+	private Date createDate;
+	
+	@Column
+	private Date modifyDate;
+	
+	public enum Gender {
+		MAN,
+		WOMAN
+	}
+	
 	public Long getId() {
 		return id;
 	}
-
-	@Column
-	private String name;
 
 	public String getName() {
 		return name;
@@ -37,5 +57,52 @@ public class Human {
 	public void setId(Long id) {
 		this.id = id;
 	}
-	
+
+	public String getMobile() {
+		return mobile;
+	}
+
+	public void setMobile(String mobile) {
+		this.mobile = mobile;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getAvatar() {
+		return avatar;
+	}
+
+	public void setAvatar(String avatar) {
+		this.avatar = avatar;
+	}
+
+	public Gender getGender() {
+		return gender;
+	}
+
+	public void setGender(Gender gender) {
+		this.gender = gender;
+	}
+
+	public Date getCreateDate() {
+		return createDate;
+	}
+
+	public void setCreateDate(Date createDate) {
+		this.createDate = createDate;
+	}
+
+	public Date getModifyDate() {
+		return modifyDate;
+	}
+
+	public void setModifyDate(Date modifyDate) {
+		this.modifyDate = modifyDate;
+	}
 }
